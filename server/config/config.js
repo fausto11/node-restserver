2d4220b9731a5fd279bb5bb5d0bba819fbd67244
fausto torres <faustotm11@outlook.com>
2020-08-08 23:06:14 -0500

    Rest Server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,19 @@
 puerto
 */
 process.env.PORT = process.env.PORT || 3000;
+/*
+entorno
+*/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+/*
+base de datos
+*/
+let urlDB;
+if(process.env.NODE_ENV==='dev'){
+    urlDB='mongodb://localhost:27017/cafe'
+}else{
+    urlDB='mongodb+srv://famongo:qp3hGeJz5B8pESFP@cluster0.tcafs.mongodb.net/cafe'
+}
+
+process.env.URLDB=urlDB;
+
